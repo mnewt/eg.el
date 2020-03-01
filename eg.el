b83be2d7f4f499b8ef1970e4f3489e211180ed82
Matthew Sojourner Newton <matt@mnewton.com>
2020-03-01 08:05:27 -0800

    fix fontification

--- eg.el
+++ eg.el
@@ -26,19 +26,19 @@
 
 ;;; Commentary:
 
-;; This is an Emacs wrapper for eg (https://github.com/srsudar/eg). From the
+;; This is an Emacs wrapper for eg (https://github.com/srsudar/eg).  From the
 ;; main repo:
 
 ;; eg provides examples of common uses of command line tools.
 
-;; Man pages are great. How does find work, again? man find will tell you, but
+;; Man pages are great.  How does find work, again? man find will tell you, but
 ;; you'll have to pore through all the flags and options just to figure out a
-;; basic usage. And what about using tar? Even with the man pages tar is
+;; basic usage.  And what about using tar? Even with the man pages tar is
 ;; famously inscrutable without the googling for examples.
 
 ;; No more!
 
-;; eg will give you useful examples right at the command line. Think of it as a
+;; eg will give you useful examples right at the command line.  Think of it as a
 ;; companion tool for man.
 
 ;; eg comes from exempli gratia, and is pronounced like the letters: "ee gee".
@@ -166,9 +166,10 @@
 
 (define-derived-mode eg-mode help-mode "eg"
   "Lookup commands using `eg'"
+  (save-excursion
+    (font-lock-fontify-region (point-min) (point-max)))
   (set (make-local-variable 'buffer-read-only) t)
-  (setq revert-buffer-function (lambda (&rest _) (eg eg-current-command)))
-  (font-lock-ensure))
+  (setq revert-buffer-function (lambda (&rest _) (eg eg-current-command))))
 
 ;; Make the definition of a "word" more expansive to we don't have false matches
 ;; when searching for commands.
